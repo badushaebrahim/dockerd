ee25f2e7de5c1ab0f1afb8bf9549564895b04cb5
badushaebrahim <bbadusha13@gmail.com>
2022-09-02 22:00:44 +0530

    implemented login and register compoenets

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Login from './comp/Login';
 import reportWebVitals from './reportWebVitals';
+import SignIn from './comp/Sighln';
+import Register from './comp/Register';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <Login/> */}
+    {/* <SignIn/> */}
+    <Register/>
   </React.StrictMode>
 );
 
